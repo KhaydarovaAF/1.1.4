adef122e0874a03e1d1d682bb238ce4839a7db9b
KhaydarovaAF <115499322+KhaydarovaAF@users.noreply.github.com>
2022-12-21 21:26:24 +0500

    задача 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static final UserService useService = new UserServiceImpl();
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+       // useService.cleanUsersTable();
+
+        useService.saveUser("Аделина", "Хайдарова", (byte) 22);
+        useService.saveUser("Василий", "Пупкин", (byte) 56);
+        useService.saveUser("Иван", "Петров", (byte) 15);
+        useService.saveUser("Петр", "Иванов", (byte) 37);
+
+        useService.removeUserById(2);
+        useService.getAllUsers();
+        useService.cleanUsersTable();
+        useService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/myschema";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "qwerty";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection OK");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
