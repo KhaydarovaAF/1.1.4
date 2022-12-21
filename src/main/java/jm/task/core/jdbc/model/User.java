53d4136d6894b392b4158805a1bdeb5c183df5d1
KhaydarovaAF <115499322+KhaydarovaAF@users.noreply.github.com>
2022-12-21 22:43:39 +0500

    Практическая задача 1.1.4

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -6,7 +6,6 @@ import javax.persistence.Table;
 
 @Table
 public class User {
-    @Id
     private Long id;
 
     @Column
@@ -58,5 +57,15 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/myschema";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "qwerty";
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Error");
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Config");
+        }
+        return connection;
+    }
 }
